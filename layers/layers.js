bae1e4dff8e269cf24b6422717ae34a37f201b5d
Geomark SIG Solutions <maumerobin38@gmail.com>
2026-02-19 13:35:48 +0400

    Update layers.js

--- layers/layers.js
+++ layers/layers.js
@@ -25,7 +25,6 @@ var lyr_Zonedevgtation_1 = new ol.layer.Vector({
                 popuplayertitle: 'Zone de végétation',
                 interactive: true,
     title: 'Zone de végétation<br />\
-lyr_Zonedevgtation_1.setOpacity(0.500000);
             
     <img src="styles/legend/Zonedevgtation_1_0.png" /> Bananeraie<br />\
     <img src="styles/legend/Zonedevgtation_1_1.png" /> Bois<br />\
